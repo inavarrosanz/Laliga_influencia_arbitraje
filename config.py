00e4d1bda1241d526bd58d7047c38eb3033ec883
inavarrosanz <inavarrosanz@gmail.com>
2026-04-21 13:46:37 +0200

    Actualizar config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Configuration file
 
-API_KEY = 'your_api_key_here'
+API_KEY = '89ec3ef5b44e473bae6c91f8fc8e7460'
 BASE_URL = 'https://api.example.com'
 COMPETITION_ID = '12345'
 OUTPUT_DIRECTORY = './output/'
